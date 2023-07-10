176ef290ec9195a5b3d6113f06d620ad91da9420
jaodoka02 <joaovbernardes02@gmail.com>
2023-07-10 16:42:45 -0300

    Exercicio de treinamente de private e default

---
+++ src/restaurante/atendimento/cozinha/Almoxarife.java
@@ -0,0 +1,18 @@
+package restaurante.atendimento.cozinha;
+
+public class Almoxarife {
+    private void controlarSaida() {
+        System.out.println("controlando saida de mercadorias");
+        }
+        
+     void entregarIngredientes() {
+        System.out.println("entregando maercadorias");
+        controlarSaida();}
+     //metodo default, chamando um metodo privado
+     void trocarGaz() {
+        System.out.println("trocando gaz");
+        }
+
+
+
+}
